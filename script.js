6eca713798430823f09255becee5181a633a6992
angelique KRAUSE <krause_angelique@hotmail.com>
2023-09-06 16:00:36 +0200

    exercice 6 à 9

--- script.js
+++ script.js
@@ -59,7 +59,6 @@ user.forEach((infoUser) => {
 });
 // Exercice 5
 // Insérez votre code ici
-
 /* Merci Sara
 let colorLigthBlue = "lightblue";
 let colorPink = "pink";
@@ -68,9 +67,11 @@ document.body.style.background = `linear-gradient(${colorPink}, ${colorLigthBlue
 let backgroundColor = "crimson";
 // Ne touchez pas à ceci
 document.body.style.backgroundColor = backgroundColor;
+
 // Insérez votre réassignation de backgroundColor ici
 backgroundColor = "lightblue";
 
+// Ne touchez pas à ceci
 document.body.style.backgroundColor = backgroundColor;
 
 // Exercice 6
@@ -79,7 +80,7 @@ document.body.style.backgroundColor = backgroundColor;
 let myImage = new Image(100, 200);
 let car = {
   brand: "peogot",
-  modele: "403",
+  model: "403",
   yearConstruct: 1956,
   color: "lightblue",
   image: myImage.src = "./Ressources/images/128.jpg",
@@ -105,6 +106,8 @@ document.getElementById("car").src = car.image;
 // const matches = document.querySelectorAll("p");
 const buttons = document.querySelectorAll("button");
 console.log(buttons);
+const cat = document.getElementById("cat");
+console.log(cat); // <img id="cat" />
 
 // Ne touche pas à ceci
 const catURL = [
@@ -133,11 +136,41 @@ buttons.forEach((btn) => {
 
 // Exercice 8
 // Insérez votre code ici
+// Sélectionnez l'élément ayant l'ID "output" en utilisant getElementById() et insérez deux nouvelles balises HTML avec innerHTML. Vous aurez besoin d'une balise "p" avec un contenu de votre choix et d'une balise "img" avec la source suivante : https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png.
+let output = document.getElementById("output");
+console.log(output);
+output.innerHTML += `<p>Hello mon p </p>
+                      <img src=" https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png" alt="JavaScript-logo" />`;
+
+/*
+Dans l'exercice TodoList
+------------------------
+ output.innerHTML += `<li>
+    <p>${taskValue}</p>
+    <div>
+      <button class="done"></button>
+      <button class="delete">
+        Delete
+      </button>
+    </div>
+  </li>`;
+*/
+
+
 
 // Exercice 9
 // Insérez votre code ici
+const fruits = ["pomme", "poire", "cerise", "banane"];
+const listOutput = document.getElementById("listOutput");
+// user.forEach((infoUser) => {
+  fruits.forEach((fruitValue) => {
+      listOutput.innerHTML += ` <li> ${fruitValue} </li>
+      `;
+  });
 
 // Exercice 10
+// Vous allez rendre l'icône affichée aléatoire lorsque vous accédez à votre page.
+
 // Ne touchez pas au tableau
 const icons = [
   "cat-solid",
@@ -151,7 +184,59 @@ const icons = [
   "shrimp-solid",
 ];
 
+/*
+ const array1 = ['a', 'b', 'c'];
+
+array1.forEach((element) => console.log(element)); 
+*/
+icons.forEach((iconValue) => {
+  console.log(iconValue);
+
+});
+/*
+let chemin = "";
+chemin = iconValue[0];
+console.log(chemin);
+*/
+
 // Insérez votre code ici
+const logo = document.getElementById("logo");
+// Pour éviter de compter le nombre d'éléments de notre tableau, créez une variable contenant la longueur de votre tableau et soustrayez 1 (car l'index commence à 0).
+const indexIcons = (icons.length) -1;
+console.log(indexIcons);
+
+// Ensuite, créez une variable qui génère un chiffre aléatoire entre 0 et 8. N'oubliez pas d'utiliser la variable précédemment créée pour l'index.
+/*
+La classe Math contient toutes les méthodes liées au nombre dans le langage JavaScript. On peut générer un nombre aléatoire en JavaScript avec la méthode Math.rand() mais ce nombre est systématiquement compris entre 0 et 1. Il faut ensuite multiplier le nombre aléatoire généré par la différence entre les deux nombres qui sont les limites et ajouter 1. On obtient ainsi un nombre à virgule. La méthode Math.floor() va récupérer la partie entière de ce nombre. Après ce calcul, il ne reste plus qu'à ajouter le nombre inférieur de l'intervalle pour obtenir un nombre aléatoire compris dans l'intervalle.
+*/
+const min = 1;
+const max = 8;
+
+function entierAleatoire(min, max)
+{
+ return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Utilisation
+//La variable contient un nombre aléatoire compris entre 1 et 8
+const entier = entierAleatoire(1, 8);
+
+
+/*let variableCheminImage = `<img id="iconImage" src="./Ressources/images/${iconValue}.svg" alt="User's Icon" />`;
+console.log(variableCheminImage);
+*/
 
 // Exercice 11 : Optionnel
+/*
+  <div>
+      <label for="iconSelect">Select an Icon:</label>
+      <select id="iconSelect">
+        <option value="">Select an icon...</option>
+        <option value="cat-solid">Cat</option>
+        <option value="shrimp-solid">Shrimp</option>
+      </select>
+      <button id="changeIcon">Change Icon</button>
+    </div>
+    <img id="iconImage" src="" alt="User's Icon" />
+*/
 // Insérez votre code ici
